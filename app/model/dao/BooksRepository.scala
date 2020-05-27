4ab8b9eed2497ce55adda6f8f90600c86feedc52
princewill ohuabunwa <wohuabun@gmail.com>
2020-05-26 21:19:53 -0400

    add crud

--- app/model/dao/BooksRepository.scala
+++ app/model/dao/BooksRepository.scala
@@ -1,27 +1,27 @@
 package model.dao
 
-import controllers.Book
+import controllers.BookInfo
 import javax.inject.Inject
 import model.Genre.Romance
-import model.{BookId, BookInfo, BookTitle}
+import model.{Book, BookId, BookTitle}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 abstract class BooksRepository {
 
-  def findByTitle(title: BookTitle): Future[Option[BookInfo]]
-  def save(bookInfo: BookInfo): Future[BookId]
+  def findById(bookId: BookId): Future[Option[Book]]
+  def save(book: Book): Future[BookId]
 }
 
 class StaticBooksRepositoryImpl @Inject()(implicit val ec: ExecutionContext) extends BooksRepository {
 
-  private var repo = scala.collection.mutable.Map("lordnap" -> BookInfo("c1fab850-648e-11e6-8b77-86f30ca893d3", Book("Lord Nap", "Jane smith", Romance, isAudioBook = false)))
+  private var repo = scala.collection.mutable.Map("c1fab850-648e-11e6-8b77-86f30ca893d3" -> Book("c1fab850-648e-11e6-8b77-86f30ca893d3", BookInfo("Lord Nap", "Jane smith", Romance, isAudioBook = false)))
 
-  override def findByTitle(title: BookTitle): Future[Option[BookInfo]] = Future.successful(repo.get(title))
+  override def findById(bookId: BookId): Future[Option[Book]] = Future.successful(repo.get(bookId))
 
-  override def save(bookInfo: BookInfo): Future[BookId] = findByTitle(bookInfo.book.title).map{
+  override def save(book: Book): Future[BookId] = findById(book.bookInfo.title).map{
     case Some(_) => throw new Exception("Books with that title already exists!")
-    case _ => repo += (bookInfo.book.title -> bookInfo); bookInfo.id
+    case _ => repo += (book.id -> book); book.id
 
   }
 
--- app/model/services/BookService.scala
+++ app/model/services/BookService.scala
@@ -1,34 +1,46 @@
 package model.services
 
 import javax.inject.Inject
-import model.{BookId, BookInfo}
+import model.{Book, BookId, Books}
 import model.dao.BookDAO
+import model.utils.ErrorException
 
 import scala.concurrent.{ExecutionContext, Future}
 
 
 trait BookService {
 
-  def addBooks(bookInfo: BookInfo): Future[BookId]
+  def addBook(book: Book): Future[BookId]
 
+  def getBooks: Future[Books]
 
-  /*def getBooks(ids: List[BookTitle]): Future[Books]
-  def getBook(id: BookTitle): Future[Book]
-  def deleteBook(id: BookTitle): Future[Ack]
-  def deleteAll(ids: List[BookTitle]): Future[Ack]
-  def updateBook(book: Book): Future[Book]*/
+  def getBook(id: BookId): Future[Book]
+
+  def deleteBook(id: BookId): Future[String]
+
+  //def updateBook(book: Book): Future[Book]
+
+  //def deleteAll(ids: List[BookTitle]): Future[Ack]
+
 
 }
 
 class BookServiceImpl @Inject()(bookDAO: BookDAO)(implicit ec: ExecutionContext) extends BookService {
 
-  def addBooks(bookInfo: BookInfo): Future[BookId] = bookDAO.save(bookInfo)
+  def addBook(book: Book): Future[BookId] = bookDAO.save(book)
 
+  def getBooks: Future[Books] =
+    bookDAO.getAll.map(_.map(Book(_)))
 
+  def getBook(id: BookId): Future[Book] =
+    bookDAO.findById(id).map {
+      case Some(dbBook) => Book(dbBook)
+      case _ => throw ErrorException(play.api.mvc.Results.BadRequest, "Book Does Not Exist!")
+    }
 
-  /*def getBooks(ids: List[BookTitle]): Future[Books]
-  def getBook(id: BookTitle): Future[Book]
-  def deleteBook(id: BookTitle): Future[Ack]
+  def deleteBook(id: BookId): Future[String] = bookDAO.delete(id)
+
+  /*
   def deleteAll(ids: List[BookTitle]): Future[Ack]
   def updateBook(book: Book): Future[Book]
 */
